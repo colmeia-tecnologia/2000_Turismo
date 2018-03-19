0ce70f4d9ca820f0883b21ee4c8896de2def5705
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-19 16:55:59 -0300

    Commit Inicial

---
+++ resources/views/painel/layout/menu.blade.php
@@ -0,0 +1,77 @@
+<ul>
+    {{--BANNERS--}}
+    @can('view-banners')
+        <li>
+            <a href='{{route('banners.index')}}' alt='Banners' title='Banners'>
+                <i class="fa fa-picture-o" aria-hidden="true"></i> Banners
+            </a>
+        </li>
+    @endcan
+    {{--PORTFOLIO--}}
+    @can('view-portfolios')
+        <li>
+            <a href='{{url('/portfolios')}}' alt='Portifólios' title='Portifólios'>
+                <i class="fa fa-camera" aria-hidden="true"></i> Portifólios
+            </a>
+        </li>
+    @endcan
+    {{--SERVICES--}}
+    @can('view-services')
+        <li>
+            <a href='{{route('services.index')}}' alt='Serviços' title='Serviços'>
+                <i class="fa fa-wrench" aria-hidden="true"></i> Serviços
+            </a>
+        </li>
+    @endcan
+    {{--CLIENTS--}}
+    @can('view-services')
+        <li>
+            <a href='{{route('clients.index')}}' alt='Clientes' title='Clientes'>
+                <i class="fa fa-users" aria-hidden="true"></i> Clientes
+            </a>
+        </li>
+    @endcan
+    {{--VIDEOS--}}
+    @can('view-videos')
+        <li>
+            <a href='{{route('videos.index')}}' alt='Videos' title='Videos'>
+                <i class="fa fa-video-camera" aria-hidden="true"></i> Videos
+            </a>
+        </li>
+    @endcan
+    {{--Users--}}
+    @can('view-users')
+        <li>
+            <a href='{{route('users.index')}}' alt='Usuários' title='Usuários'>
+                <i class="fa fa-user" aria-hidden="true"></i> Usuários
+            </a>
+        </li>
+    @endcan
+    {{--Users--}}
+    <li>
+        <a href='{{url('/users/'.Auth::user()->id.'/edit')}}' alt='Meus dados' title='Meus dados'>
+            <i class="fa fa-user" aria-hidden="true"></i> Meus dados
+        </a>
+    </li>
+
+    {{--BLOG--}}
+    <li class='menuLabel'>
+        Blog
+    </li>
+    {{--POST_CATEGORIES--}}
+    @can('view-post_categories')
+        <li>
+            <a href='{{route('post_categories.index')}}' alt='Categorias Post' title='Categorias Post'>
+                <i class="fa fa-list" aria-hidden="true"></i> Categorias Post
+            </a>
+        </li>
+    @endcan
+    {{--POSTS--}}
+    @can('view-posts')
+        <li>
+            <a href='{{url('/posts')}}' title='Posts' alt='Posts' title='Posts'>
+                <i class="fa fa-file-text"></i> Posts
+            </a>
+        </li>
+    @endcan
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+/*
+ * PAINEL
+ */
+Route::group([
+                'domain' => 'painel.'.str_replace('http://','',env('APP_URL')),
+                'namespace' => 'Painel',
+                'middleware' => 'auth'
+            ], function() 
+{
+    //Painel
+    Route::get('/', 'PainelController@index');
+    Route::resource('banners', 'BannerController');
+    Route::resource('services', 'ServiceController');
+    Route::resource('portfolios', 'PortfolioController');
+    Route::resource('clients', 'ClientController');
+    Route::resource('videos', 'VideoController');
+    Route::resource('users', 'UserController');
+    
+    //Blog
+    Route::resource('post_categories', 'PostCategoryController');
+    Route::resource('posts', 'PostController');
+
+
+    //Validações Video
+    Route::post('videos/verificaUrlCurta', 'VideoController@verifyShortenUrl')->name('videos.verificaUrlCurta');
+    Route::post('videos/verificaUrlYoutubeValida', 'VideoController@verifyValidYoutubeUrl')->name('videos.verificaUrlYoutubeValida');
+    Route::post('videos/obtemImagemYoutube', 'VideoController@getYoutubeThumb')->name('videos.obtemImagemYoutube');
+
+    //Upload
+    Route::get('upload', 'UploadController@index');
+    Route::post('upload/upload', 'UploadController@upload')->name('upload.upload');
+    Route::get('upload/delete/{file}', 'UploadController@delete')->name('upload.delete');
+
+    //Ativo/Inativo
+    Route::post('/activate-inactivate', 'ActivateController@activateInactivate')->name('activate-inactivate');
+});
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
